37f523ae9bfbe1c81810dc4e90512421e6b666f3
sudhanshumishra <sudhanshu.mishra@ippayments.com>
2015-10-26 10:58:31 +1100

    Initial checkin with implementation of RangeSet and basic tests

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # RangeSet
 Implementation of a data structure that can map a range of values to a single value
+
+## Synopsis
+
+## Code Example
+```csharp
+using Smi.RangeSet.Lib;
+
+// Create an collection of Range and their values
+var seedCollection = new List<KeyValuePair<Range, int>>
+			{
+				new KeyValuePair<Range, int>(new Range(3000, 3999), 1),
+				new KeyValuePair<Range, int>(new Range(4000, 4999), 2),
+				new KeyValuePair<Range, int>(new Range(6000, 6999), 3)
+			};
+// Instantiate RangeSet
+RangeSet rangeSet = new RangeSet(seedCollection);
+
+// Lookup the value of a range
+int value = rangeSet.GetValueOfItem("3256"); // returns 1
+
+int anotherValue = rangeSet.GetValueOfItem("7000"); // returns 0 as no range exists that contains 7000
+```
+
+## Contributors
+
+@sudhanshutheone.
+
+## License
+
+MIT License
---
+++ src/Smi.RangeSet.Lib.Tests/RangesSetTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace Smi.RangeSet.Lib.Tests
+{
+	[TestClass]
+	public class RangesSetTest
+	{
+		private static List<KeyValuePair<Range, int>> _listOfRanges;
+		private static RangeSet _rangeSet;
+
+		[ClassInitialize]
+		public static void TestInit(TestContext testContext)
+		{
+			string json = GetTextFromEmbededFile("Smi.RangeSet.Lib.Tests.RangeMap.json");
+			_listOfRanges = JsonConvert.DeserializeObject<List<KeyValuePair<Range, int>>>(json);
+			_rangeSet = new RangeSet(_listOfRanges);
+		}
+
+		[TestMethod, TestCategory("Unit")]
+		public void Range_ShouldNotAccept_HighLessThanLow()
+		{
+			Action act = () => new Range(2356, 2300);
+
+			act.ShouldThrow<ArgumentOutOfRangeException>(
+				"because we expect Range to instantiate only if lower bound is less than or equal to upper bound");
+		}
+
+		[TestMethod, TestCategory("Unit")]
+		public void RangeSet_CanCorrectly_GetValueOfMappedRange()
+		{
+			int value = _rangeSet.GetValueOfItem("3012");
+
+			value.Should().Be(6, "because we know we passed an element that is mapped to 6");
+		}
+
+		[TestMethod, TestCategory("Unit")]
+		public void RangeSet_ReturnsZero_ForAnUnmappedRange()
+		{
+			int value = _rangeSet.GetValueOfItem("2356");
+			value.Should().Be(0, "because we have passed an unmapped item to find");
+		}
+
+		private static string GetTextFromEmbededFile(string fileName)
+		{
+			var assembly = Assembly.GetExecutingAssembly();
+			var resourceName = fileName;
+			var result = string.Empty;
+
+			using (var stream = assembly.GetManifestResourceStream(resourceName))
+			{
+				if (stream == null) return result;
+				var reader = new StreamReader(stream);
+				result = reader.ReadToEnd();
+
+			}
+
+			return result;
+		}
+	}
+}
