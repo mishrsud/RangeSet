2eaed91e9ac0371cfc2d4dbb6d0f3afab4ef410e
sudhanshumishra <sudhanshu.mishra@ippayments.com>
2015-10-26 14:03:46 +1100

    Added missing project

---
+++ src/Smi.RangeSet.Lib/Range.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Smi.RangeSet.Lib
+{
+	/// <summary> Encapsulates an item that has a lower bound and an upper bound </summary>
+	public class Range
+	{
+		private readonly int _low;
+		private readonly int _high;
+
+		/// <summary> Gets the lower bound of the item. </summary>
+		public int Low
+		{
+			get
+			{
+				return this._low;
+			}
+		}
+
+		/// <summary> Gets the upper bound of the item. </summary>
+		public int High
+		{
+			get
+			{
+				return this._high;
+			}
+		}
+
+		/// <summary> 
+		/// Initializes a new instance of the <see cref="Range" /> class.
+		/// </summary>
+		/// <param name="low">The lower bound.</param>
+		/// <param name="high">The upper bound.</param>
+		/// <exception cref="System.ArgumentOutOfRangeException">high;the upper bound of range should be greater than or equal to the lower bound</exception>
+		public Range(int low, int high)
+		{
+			if (high < low) throw new ArgumentOutOfRangeException("high", "the upper bound of range should be greater than or equal to the lower bound");
+			this._low = low;
+			this._high = high;
+		}
+
+		/// <summary> Determines whether this instance of range contains the specified element. </summary>
+		/// <param name="element">The element.</param>
+		/// <returns>true if the element is contained in the Range, otherwise false. </returns>
+		public bool Contains(int element)
+		{
+			return element >= this.Low && element <= this.High;
+		}
+	}
+}
---
+++ src/Smi.RangeSet.Lib/RangeSet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Smi.RangeSet.Lib
+{
+	/// <summary>
+	/// Encapsulates a collection of key value pairs where the key is a <see cref="Range"/> and the value is a single type
+	/// </summary>
+	public class RangeSet
+	{
+		private readonly List<KeyValuePair<Range, int>> _ranges;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="RangeSet"/> class.
+		/// </summary>
+		/// <param name="ranges">The ranges.</param>
+		public RangeSet(List<KeyValuePair<Range, int>> ranges)
+		{
+			this._ranges = ranges;
+		}
+
+		/// <summary> 
+		/// Gets the value that is mapped to the range to which the item belongs or 0 if no range containing the item is found. 
+		/// </summary>
+		/// <param name="itemToFindInRange">The item to find in range.</param>
+		/// <returns></returns>
+		/// <exception cref="ArgumentOutOfRangeException">itemToFindInRange;Invalid item passed in. An integer is expected.</exception>
+		public int GetValueOfItem(string itemToFindInRange)
+		{
+			int itemToFind;
+			if (!int.TryParse(itemToFindInRange, out itemToFind))
+			{
+				throw new ArgumentOutOfRangeException("itemToFindInRange", "Invalid item passed in. An integer is expected.");
+			}
+
+			var result = _ranges.FirstOrDefault(kvp => kvp.Key.Contains(itemToFind)).Value;
+			
+			return result;
+		}
+	}
+}
